cc995459cc5601f2686d62973434ecb47f9b2504
gustavoLimar <131462760+gustavoLimar@users.noreply.github.com>
2023-05-02 23:06:38 -0300

    Add files via upload

---
+++ beecrowd 1060.py
@@ -0,0 +1,23 @@
+a = float(input())
+b = float(input())
+c = float(input())
+d = float(input())
+e = float(input())
+f = float(input())
+
+aa = 0
+
+if a > 0:
+    aa = aa + 1
+if b > 0:
+    aa = aa + 1
+if c > 0:
+    aa = aa + 1
+if d > 0:
+    aa = aa + 1
+if e > 0:
+    aa = aa + 1
+if f > 0:
+    aa = aa + 1
+
+print(f"{aa} valores positivos")
